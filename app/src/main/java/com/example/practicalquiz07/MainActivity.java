417fc1c6f231d6a47a44d59c081f1d835e0b5e49
17045697 <17045697@myrp.edu.sg>
2019-06-06 16:17:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practicalquiz07/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.practicalquiz07;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRetrieve;
+    TextView tvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        tvText = findViewById(R.id.tvText);
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS);
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, 0);
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"address", "body"};
+                ContentResolver cr = getContentResolver();
+                String filter = "address LIKE ? AND body LIKE ?";
+                String[] filterArgs = {"%66%", "%RP"};
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+
+                        String address = cursor.getString(0);
+                        String body = cursor.getString(1);
+
+                        smsBody += address + ": "+ body + "\n" ;
+
+
+                    } while (cursor.moveToNext());
+                }
+                tvText.setText(smsBody);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode){
+            case 0 :{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    btnRetrieve.performClick();
+                }else{
+                    Toast.makeText(MainActivity.this, "Permisson not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+}
+
+
+
